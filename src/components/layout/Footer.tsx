1e4d2fa5d4f1ae2fc74fbe0b76b0f5ea962c16a8
Abhinav-THAJ <98870669+Abhinav-THAJ@users.noreply.github.com>
2026-08-28 14:11:38 +0530

    Fix lucide-react build error in Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Facebook, Twitter, Instagram, Youtube, MapPin, Phone, Mail } from "lucide-react";
+import { MapPin, Phone, Mail } from "lucide-react";
 
 export function Footer() {
   return (
